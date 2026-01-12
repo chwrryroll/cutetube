cd8424e3c97015fd44a77b7d5d0fe9e2f44c2890
Charity <cherryroll@proton.me>
2026-01-12 21:37:44 +0300

    Added a new component for videos, and a page to scroll them

---
+++ app/components/video/index.vue
@@ -0,0 +1,29 @@
+<style lang="scss">
+  @use "./style.sass";
+</style>
+<script setup lang="ts">
+  const likes = useState("likes", () => Math.round(Math.random() * 1000))
+</script>
+<template>
+  <div class="video">
+    <div class="container">
+      <video class="video__player" autoplay loop @dblclick="likes++">
+        <slot />
+      </video>
+      <div class="video__reactions">
+        <button class="reactions__button" @click="likes++">
+          <Icon class="button__icon" name="solar:heart-linear"/>
+          <p class="button__label">{{likes}}</p>
+        </button>
+        <button class="reactions__button">
+          <Icon class="button__icon" name="solar:chat-round-linear"/>
+          <p class="button__label">2K</p>
+        </button>
+        <button class="reactions__button">
+          <Icon class="button__icon" name="solar:plain-linear"/>
+          <p class="button__label">2K</p>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app/pages/video/[id].vue
@@ -0,0 +1,52 @@
+<style lang="scss">
+@use "./style.sass";
+</style>
+<script setup lang="tsx">
+  /** @jsxImportSource vue */
+  import Video from '~/components/video/index.vue'
+  import m, { motion, useScroll, useSpring, useTransform } from 'motion-v'
+  import { ref, defineComponent } from 'vue'
+  import { createRouter, createWebHashHistory } from 'vue-router'
+
+  function useParallax(
+    value    : m.MotionValue<number>,
+    distance : number
+  ) {
+    return useTransform(value, [0, 1], [-distance, distance])
+  }
+
+  // TODO!
+  // const router = createRouter({
+  //   history: createWebHashHistory(),
+  //   routes: [],
+  //   scrollBehavior (to, from, savedPosition) {
+  //     return desired position
+  //   }
+  // })
+
+  const Scroller = defineComponent({
+    props: {
+      url: { type: String, required: true }
+    },
+    setup(props) {
+      const targetRef = ref(null)
+      return () => (
+        <section>
+          <div ref={targetRef}>
+            <Video>
+              <source src={props.url} type="video/mp4" />
+            </Video>
+          </div>
+        </section>
+      )
+    }
+  })
+</script>
+<template>
+  <div class="w-screen flex h-lvh">
+    <div class="scroller">
+      <Scroller class="vcontainer" v-for="image in ['/bunny.mp4', '/bunny.mp4']" :url="image" />
+    </div>
+  </div>
+  <Tabbar/>
+</template>
