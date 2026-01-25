1d4da79eca97c2b7f9e9f8a7cf8250f4e786070e
Charity <cherryroll@proton.me>
2026-01-25 15:27:17 +0300

    Cleaned <NotifyBlock/>'s styling a little

--- app/components/notify/block/index.vue
+++ app/components/notify/block/index.vue
@@ -1,3 +1,6 @@
+<style lang="sass">
+  @use "./style.sass"
+</style>
 <script setup lang="ts">
   type Action = 'like' | 'comment'
   const props = defineProps<{
@@ -7,14 +10,14 @@
 </script>
 <template>
   <div>
-    <div class="notify__block flex items-center p-4 pb-2">
-      <div class="size-8 rounded-full bg-red-400 relative z-20"></div>
-      <p class="text-gray-600 text-sm ml-2">
+    <div class="notify__block">
+      <div class="block__avatar"></div>
+      <p class="block__content">
         <span class="text-black mr-2">@{{props.username}}</span>
         <span v-if="props.action == 'like'">liked your post</span>
         <span v-if="props.action == 'comment'">commented</span>
       </p>
-      <p class="ml-auto text-xs opacity-50">1h ago</p>
+      <p class="block__timestamp">1h ago</p>
     </div>
     <div v-if="props.action == 'comment'" class="pb-2">
       <div class="bg-gray-300 rounded-xl mx-2 p-2">
